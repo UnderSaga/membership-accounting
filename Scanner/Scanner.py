c49a30409fb29f6212e24370529c84ea2a05d9a1
UnderSaga <undersaga@mail.ru>
2022-02-23 17:13:30 +0300

    Сканер готов, обрабатывает только code128, после сканирования сразу закрывается

--- Scanner/Scanner.py
+++ Scanner/Scanner.py
@@ -1,11 +1,7 @@
 from pyzbar import pyzbar
 import cv2
 
-
 def draw_barcode(decoded, image):
-    # n_points = len(decoded.polygon)
-    # for i in range(n_points):
-    #     image = cv2.line(image, decoded.polygon[i], decoded.polygon[(i+1) % n_points], color=(0, 255, 0), thickness=5)
     image = cv2.rectangle(image, (decoded.rect.left, decoded.rect.top),
                         (decoded.rect.left + decoded.rect.width, decoded.rect.top + decoded.rect.height),
                         color=(0, 255, 0),
@@ -13,16 +9,17 @@ def draw_barcode(decoded, image):
     return image
 
 def decode(image):
-    # decodes all barcodes from an image
+    code128 = "CODE128"
     decoded_objects = pyzbar.decode(image)
     for obj in decoded_objects:
-        # draw the barcode
-        image = draw_barcode(obj, image)
-        # print barcode type & data
-        print("Type:", obj.type)
-        print("Data:", obj.data)
-        print()
-        break
+        if obj.type == code128:
+            cleartype = str(obj.data).find('\'')
+            data = str(obj.data)[(cleartype + 1):-1]
+            print(data)
+            raise SystemExit
+        else:
+            print('Wrong type')
+            raise SystemExit
     return image
 
 def camera():
