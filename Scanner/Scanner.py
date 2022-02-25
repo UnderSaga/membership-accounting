dc516cb3b2fa4c67d5cb97366c17d07fc196bced
UnderSaga <undersaga@mail.ru>
2022-02-25 14:01:50 +0300

    Убрана лишняя функция, добавлена надпись в случае неверного типа штрих-кода

--- Scanner/Scanner.py
+++ Scanner/Scanner.py
@@ -1,26 +1,21 @@
 from pyzbar import pyzbar
 import cv2
 
-def draw_barcode(decoded, image):
-    image = cv2.rectangle(image, (decoded.rect.left, decoded.rect.top),
-                        (decoded.rect.left + decoded.rect.width, decoded.rect.top + decoded.rect.height),
-                        color=(0, 255, 0),
-                        thickness=5)
-    return image
 
 def decode(image):
-    code128 = "CODE128"
     decoded_objects = pyzbar.decode(image)
     for obj in decoded_objects:
-        if obj.type == code128:
+        if obj.type == "CODE128":
             cleartype = str(obj.data).find('\'')
             data = str(obj.data)[(cleartype + 1):-1]
             print(data)
             raise SystemExit
         else:
-            print('Wrong type')
-            raise SystemExit
+            cv2.putText(image, 'Wrong type',
+                        (445, 470), cv2.FONT_HERSHEY_COMPLEX,
+                        1, (0, 0, 0), 2)
     return image
+
 
 def camera():
     cap = cv2.VideoCapture(0)
@@ -30,6 +25,7 @@ def camera():
         cv2.imshow("frame", frame)
         if cv2.waitKey(1) == 27:
             break
+
 
 if __name__ == '__main__':
     camera()
